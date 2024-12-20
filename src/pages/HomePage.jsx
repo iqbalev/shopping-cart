e3d8230d17d697989795d7612fbd783f7975fb1d
Muhammad Iqbal <ytsejam09.03.00@gmail.com>
2024-12-20 08:29:51 +0700

    fix: replace button wrapping Link with styled Link

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,12 +4,12 @@ function HomePage() {
   return (
     <section className="grid place-content-center gap-4 h-screen p-8">
       <h2 className="text-2xl font-bold uppercase">Welcome to our shop!</h2>
-      <button
-        type="button"
-        className="rounded-sm border border-neutral-800 p-2 text-neutral-800"
+      <Link
+        to="shop"
+        className="rounded-sm border border-neutral-800 p-2 text-neutral-800 text-center"
       >
-        <Link to="shop">Shopping Now</Link>
-      </button>
+        Shopping Now
+      </Link>
     </section>
   );
 }
