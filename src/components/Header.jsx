6ebb1c88d2aac609c456a4882e8e5786fad511fa
Muhammad Iqbal <ytsejam09.03.00@gmail.com>
2024-12-23 16:33:55 +0700

    feat: implement hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,33 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import NavBar from "./NavBar";
 import PropTypes from "prop-types";
 
 function Header({ quantities }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const cartItemCount = Object.values(quantities).reduce(
     (sum, itemQuantity) => sum + itemQuantity,
     0
   );
 
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
-    <header className="flex flex-col gap-4 items-center py-4 sm:flex-row justify-around">
+    <header className="flex p-4 justify-between gap-4 items-center sm:justify-around">
       <h1
         role="button"
         className="text-2xl font-bold uppercase tracking-widest button"
       >
         <Link to="/">Shopping App</Link>
       </h1>
-      <NavBar cartItemCount={cartItemCount} />
+      <NavBar
+        cartItemCount={cartItemCount}
+        isMenuOpen={isMenuOpen}
+        toggleMenu={toggleMenu}
+      />
     </header>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,21 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-function NavBar({ cartItemCount }) {
+function NavBar({ cartItemCount, isMenuOpen, toggleMenu }) {
   return (
-    <nav>
-      <ul className="flex gap-6">
+    <nav className="relative">
+      <button type="button" onClick={toggleMenu} className="sm:hidden">
+        <span className="block w-6 h-0.5  bg-neutral-800 mb-1"></span>{" "}
+        <span className="block w-6 h-0.5 bg-neutral-800 mb-1"></span>{" "}
+        <span className="block w-6 h-0.5  bg-neutral-800"></span>
+      </button>
+      <ul
+        className={
+          !isMenuOpen
+            ? "hidden gap-6 sm:flex"
+            : "absolute p-2 top-8 right-0 w-24 bg-neutral-800 text-white z-20 gap-4 border rounded-sm sm:flex sm:static sm:z-auto sm:text-black sm:bg-transparent sm:border-none sm:transform-none sm:w-auto"
+        }
+      >
         <li>
           <Link to="shop">Shop</Link>
         </li>
@@ -20,6 +31,8 @@ function NavBar({ cartItemCount }) {
 
 NavBar.propTypes = {
   cartItemCount: PropTypes.number.isRequired,
+  isMenuOpen: PropTypes.bool.isRequired,
+  toggleMenu: PropTypes.func.isRequired,
 };
 
 export default NavBar;
