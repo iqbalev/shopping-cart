f2490b39fb28c484a3730fad5efb52dd51027c54
Muhammad Iqbal <52034320+iqbalev@users.noreply.github.com>
2024-12-27 21:13:08 +0700

    styles: set NavLink to uppercase, bold, add letter spacing, underline, and hover effect

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -7,7 +7,7 @@ function NavMenu({ isMobileNavMenuOpen, cartItemCount, closeMobileNavMenu }) {
     <ul
       className={
         !isMobileNavMenuOpen
-          ? "hidden sm:block"
+          ? "hidden sm:block sm:p-4"
           : "fixed sm:static h-full sm:h-auto w-2/5 sm:w-auto top-0 right-0 p-4 z-10 sm:z-auto border sm:border-none rounded-sm bg-white sm:bg-transparent"
       }
     >
@@ -16,12 +16,28 @@ function NavMenu({ isMobileNavMenuOpen, cartItemCount, closeMobileNavMenu }) {
       </div>
       <div className="flex flex-col sm:flex-row sm:gap-6">
         <li>
-          <NavLink to="shop" onClick={closeMobileNavMenu}>
+          <NavLink
+            to="shop"
+            onClick={closeMobileNavMenu}
+            className={({ isActive }) =>
+              `hover:text-neutral-700 uppercase font-bold tracking-wider ${
+                isActive ? "underline underline-offset-4" : ""
+              }`
+            }
+          >
             Shop
           </NavLink>
         </li>
         <li>
-          <NavLink to="cart" onClick={closeMobileNavMenu}>
+          <NavLink
+            to="cart"
+            onClick={closeMobileNavMenu}
+            className={({ isActive }) =>
+              `hover:text-neutral-700 uppercase font-bold tracking-wider ${
+                isActive ? "underline underline-offset-4" : ""
+              }`
+            }
+          >
             {cartItemCount !== 0 ? `Cart (${cartItemCount})` : "Cart"}
           </NavLink>
         </li>
