ac22d95564b328520493ab17bf5bc398f847fb7f
Muhammad Iqbal <52034320+iqbalev@users.noreply.github.com>
2025-01-03 16:08:11 +0700

    feat: add an animated spinner to Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,10 @@
 function Loader() {
-  return <p>Loading products...</p>;
+  return (
+    <div className="flex justify-center items-center gap-4">
+      <div className="animate-spin w-8 h-8 rounded-full border border-x-2 border-y-2 border-b-indigo-500"></div>
+      <p>Loading...</p>
+    </div>
+  );
 }
 
 export default Loader;
